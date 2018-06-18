702851b7989e6129f4ade9524628272e06d4cd2b
Sanket <sanket@fete.la>
2018-06-18 11:18:33 -0400

    3 routes

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ app.get('/', (req,res)=>{
     res.send('Hay');
 })
 
+// Contacts List page: show the user all contacts
+app.get('/contacts', (req,res)=>{
+    res.send('you are on the listing page');
+})
+
+// contacts deatils 
+app.get('/contacts/:id',(req,res)=>{
+    res.send(`you are viewing deets for ${req.params.id}`);
+
+})
+
+
+
 app.listen(8888,()=> {
     console.log('your express app is running at localhost');
 })
+
